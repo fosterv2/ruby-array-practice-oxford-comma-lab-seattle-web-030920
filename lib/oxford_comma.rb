597bc9d265463126dc2504a1ae82fb88bb9b7f3e
Valerie Foster <fosterv2@outlook.com>
2020-03-28 18:57:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,8 @@ def oxford_comma(array)
   # end
   if array.length == 1
     array[0]
+  elsif array.length == 2
+    array.join(", and ")
   else
     ending = array.pop
     "#{array.join(", ")}, and #{ending}"
